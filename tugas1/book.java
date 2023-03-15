4d97521f79aa0954c62836eb446f6cb4ac17786c
arifamhrni <arifamaharanisalsabil@gmail.com>
2023-03-16 00:18:28 +0700

    tugas pemlan

--- tugas1/book.java
+++ tugas1/book.java
@@ -1,34 +1,38 @@
 package tugas1;
 
-class book {
+//created by Arifa Maharani Salsabil
+//kelas ti-e
+//PemLan
+
+public class book {
     private String kategori;
     private String judul;
-    private String namaPenulis;
-    private int jumlahHalaman;
-    private int tahunTerbit;
-
-    public void setKategori (String s) {
-        kategori = s;
+    private int tahunterbit;
+    private int jumlahhalaman;
+    public void setKategori(String kategori){
+        this.kategori = kategori;
     }
-    public void setJudul (String s) {
-        judul = s;
+    public void setJudul(String judul){
+        this.judul = judul;
     }
-    public void setNamaPenulis (String s){
-        namaPenulis =  s;
+    public void setTahunTerbit(int i){
+        this.tahunterbit = i;
     }
-    public void setJumlahHalaman (int i){
-        jumlahHalaman = i;
+    public void setJumlahHalaman(int i) {
+        this.jumlahhalaman = i;
     }
-    public void setTahunTerbit (int i){
-        tahunTerbit = i;
-    }
-    public void displayMesssage(){
-        System.out.println("Buku anda berkategori = " + kategori);
-        System.out.println("dengan Judul = " + judul);
-        System.out.println("dengan nama penulis = " + namaPenulis);
-        System.out.println("dengan jumlah halaman = " + jumlahHalaman);
-        System.out.println("dengan tahun terbit = " + tahunTerbit);
+    public void setNamaPenulis(String penulis) {
     }
 
- }
-    
+    public void tampilkan(){
+        System.out.println("====================================");
+        System.out.println("kategori buku : " + kategori);
+        System.out.println("1. Judul Buku : " + judul);
+        System.out.println("2. Tahun terbit : " + tahunterbit);
+        System.out.println("3. Jumlah halaman : " + jumlahhalaman);
+        System.out.println("====================================");
+        System.out.println();
+    }
+	public void displayMesssage() {
+	}
+}
--- tugas1/mainbook.java
+++ tugas1/mainbook.java
@@ -1,15 +1,305 @@
 package tugas1;
+//created by Arifa Maharani Salsabil
+//NIM 225150701111034
+//kelas TI-E
+//PemLan
 
 public class mainbook {
     public static void main(String[] args) {
-        book m1 = new book();
-        m1.setKategori("teknologi");
-        m1.setJudul("Pengantar Teknologi");
-        m1.setNamaPenulis("Nadin Amizah");
-        m1.setJumlahHalaman(133);
-        m1.setTahunTerbit(2009);
-        System.out.println("==================");
+        book t1 = new book();
+        t1.setKategori("teknologi");
+        t1.setJudul("Buku Dasar-Dasar Teknik Informatika");
+        t1.setNamaPenulis("Novega Pratama Adiputra");
+        t1.setJumlahHalaman(117);
+        t1.setTahunTerbit(2020);
 
-        m1.displayMesssage();
+        book t2 = new book();
+        t2.setKategori("teknologi");
+        t2.setJudul("Buku Pengantar Teknologi Informasi");
+        t2.setNamaPenulis("Buhoori Muslim");
+        t2.setJumlahHalaman(329);
+        t2.setTahunTerbit(2017);
+
+        book t3 = new book();
+        t3.setKategori("teknologi");
+        t3.setJudul("Metode Penelitian Teknik Informatika");
+        t3.setNamaPenulis("Ade Johar Maturidi");
+        t3.setJumlahHalaman(135);
+        t3.setTahunTerbit(2009);
+
+        book t4 = new book();
+        t4.setKategori("teknologi");
+        t4.setJudul("Komputer Cerdas Untuk Mahasiswa Teknik Informatika");
+        t4.setNamaPenulis("Nur Nafi’iyah");
+        t4.setJumlahHalaman(243);
+        t4.setTahunTerbit(2017);
+
+        book t5 = new book();
+        t5.setKategori("teknologi");
+        t5.setJudul("Buku Aplikasi Komputer");
+        t5.setNamaPenulis("Dwi Krisbiantoro");
+        t5.setJumlahHalaman(167);
+        t5.setTahunTerbit(2018);
+
+
+        book f1 = new book();
+        f1.setKategori("filsafat,");
+        f1.setJudul("Buku Ajar Filsafat Ilmu");
+        f1.setNamaPenulis("Drs. H. Agus Thoha, M.Si.");
+        f1.setJumlahHalaman(158);
+        f1.setTahunTerbit(2020);
+
+        book f2 = new book();
+        f2.setKategori("filsafat,");
+        f2.setJudul("Buku Studi Dasar Filsafat");
+        f2.setNamaPenulis("Tazkiyah Basaad");
+        f2.setJumlahHalaman(187);
+        f2.setTahunTerbit(2018);
+
+        book f3 = new book();
+        f3.setKategori("filsafat");
+        f3.setJudul("Buku Fislafat Hukum");
+        f3.setNamaPenulis("Uri Sucipto");
+        f3.setJumlahHalaman(136);
+        f3.setTahunTerbit(2019);
+
+        book f4 = new book();
+        f4.setKategori("filsafat,");
+        f4.setJudul("Telaah Filsafat Pendidikan");
+        f4.setNamaPenulis("Aliet Noorhayati Sutisno, Fikriyah, Windari Pandanita");
+        f4.setJumlahHalaman(180);
+        f4.setTahunTerbit(2019);
+
+        book f5 = new book();
+        f5.setKategori("filsafat,");
+        f5.setJudul("Hakikat Mencari Pengetahuan");
+        f5.setNamaPenulis("Ahmad Taufik Nasution");
+        f5.setJumlahHalaman(134);
+        f5.setTahunTerbit(2009);
+
+
+        book s1 = new book();
+        s1.setKategori("sejarah");
+        s1.setJudul("Di Bawah Bendera Revolusi");
+        s1.setNamaPenulis("Ir. Soekarno");
+        s1.setJumlahHalaman(699);
+        s1.setTahunTerbit(1965);
+
+        book s2 = new book();
+        s2.setKategori("sejarah");
+        s2.setJudul("A History of Modern Indonesia");
+        s2.setNamaPenulis("Adrian Vickers");
+        s2.setJumlahHalaman(291);
+        s2.setTahunTerbit(2005);
+
+        book s3 = new book();
+        s3.setKategori("sejarah");
+        s3.setJudul("Nusantara: Sejarah Indonesia");
+        s3.setNamaPenulis("Vlekke Bernard, Mardi Siswoko Bakti, Samsudin Berlian Zaim Rofiqi");
+        s3.setJumlahHalaman(528);
+        s3.setTahunTerbit(2008);
+
+        book s4 = new book();
+        s4.setKategori("sejarah");
+        s4.setJudul("Unfinished Nation: Indonesia Before and After Soeharto");
+        s4.setNamaPenulis("Max Lane");
+        s4.setJumlahHalaman(312);
+        s4.setTahunTerbit(2008);
+
+        book s5 = new book();
+        s5.setKategori("sejarah");
+        s5.setJudul("Laut Bercerita");
+        s5.setNamaPenulis("Leila S. Chudori");
+        s5.setJumlahHalaman(380);
+        s5.setTahunTerbit(2017);
+
+
+        book a1 = new book();
+        a1.setKategori("agama");
+        a1.setJudul("Nalar Tasawuf Sebagai Revolusi Pendidikan Islam");
+        a1.setNamaPenulis("Istania Widayati Hidayati");
+        a1.setJumlahHalaman(85);
+        a1.setTahunTerbit(2019);
+
+        book a2 = new book();
+        a2.setKategori("agama");
+        a2.setJudul("Komunikasi dan Dakwah Islam");
+        a2.setNamaPenulis("Abdul Pirol");
+        a2.setJumlahHalaman(162);
+        a2.setTahunTerbit(2018);
+
+        book a3 = new book();
+        a3.setKategori("agama");
+        a3.setJudul("Murabahah");
+        a3.setNamaPenulis("Mukhlishin");
+        a3.setJumlahHalaman(212);
+        a3.setTahunTerbit(2018);
+
+        book a4 = new book();
+        a4.setKategori("agama");
+        a4.setJudul("17 Tuntunan Hidup Muslim");
+        a4.setNamaPenulis("Wahyono Hadi Parmono dan Ismunandar");
+        a4.setJumlahHalaman(393);
+        a4.setTahunTerbit(2017);
+
+        book a5 = new book();
+        a5.setKategori("agama");
+        a5.setJudul("Bahagia Bersama Qur’an");
+        a5.setNamaPenulis("Dasep Bayu Ahyar");
+        a5.setJumlahHalaman(357);
+        a5.setTahunTerbit(2019);
+
+
+        book  p1 = new book();
+        p1.setKategori("psikologi");
+        p1.setJudul("Buku Bimbingan dan Konseling Belajar");
+        p1.setNamaPenulis("Abdul Saman & Agustan Arifin");
+        p1.setJumlahHalaman(173);
+        p1.setTahunTerbit(2018);
+
+
+        book p2 = new book();
+        p2.setKategori("psikologi");
+        p2.setJudul("Buku Psikologi Pedidikan");
+        p2.setNamaPenulis("Mahsup & Yunita Septriana Anwar");
+        p2.setJumlahHalaman(152);
+        p2.setTahunTerbit(2018);
+
+        book p3 = new book();
+        p3.setKategori("psikologi");
+        p3.setJudul("Buku Psikologi Belajar");
+        p3.setNamaPenulis("Afi Parnawi");
+        p3.setJumlahHalaman(160);
+        p3.setTahunTerbit(2019);
+
+        book p4 = new book();
+        p4.setKategori("psikologi");
+        p4.setJudul("Buku Psikologi Komunikasi");
+        p4.setNamaPenulis("Markus Utomo Sukendar");
+        p4.setJumlahHalaman(76);
+        p4.setTahunTerbit(2017);
+
+        book p5 = new book();
+        p5.setKategori("psikologi");
+        p5.setJudul("Buku Manusia Dalam Pandangan Psikologi");
+        p5.setNamaPenulis("Pasiska, M.A. Takdir Alisyahbana, M.Pd.I");
+        p5.setJumlahHalaman(148);
+        p5.setTahunTerbit(2020);
+
+
+        book l1 = new book();
+        l1.setKategori("politik");
+        l1.setJudul("Politik Hukum di Indonesia");
+        l1.setNamaPenulis("Prof. Dr. Moh. Mahfud MD");
+        l1.setJumlahHalaman(405);
+        l1.setTahunTerbit(2018);
+
+        book l2 = new book();
+        l2.setKategori("politik");
+        l2.setJudul("Etika Politik");
+        l2.setNamaPenulis("Franz Magnis Suseno");
+        l2.setJumlahHalaman(405);
+        l2.setTahunTerbit(1994);
+
+        book l3 = new book();
+        l3.setKategori("politik");
+        l3.setJudul("Dasar-Dasar Ilmu Politik");
+        l3.setNamaPenulis("Miriam Budiardjo");
+        l3.setJumlahHalaman(517);
+        l3.setTahunTerbit(2008);
+
+        book l4 = new book();
+        l4.setKategori("politik");
+        l4.setJudul("Negara dan Politik Kesejahteraan");
+        l4.setNamaPenulis("A. Muhaimin Iskandar");
+        l4.setJumlahHalaman(336);
+        l4.setTahunTerbit(2021);
+
+        book l5 = new book();
+        l5.setKategori("politik");
+        l5.setJudul("Dramaturgi Politik Indonesia");
+        l5.setNamaPenulis("Muhammad Sulhan");
+        l5.setJumlahHalaman(142);
+        l5.setTahunTerbit(2021);
+
+
+        book k1 = new book();
+        k1.setKategori("fiksi");
+        k1.setJudul("Brianna dan Bottomwise");
+        k1.setNamaPenulis("Andrea Hirata");
+        k1.setJumlahHalaman(133);
+        k1.setTahunTerbit(2030);
+
+        book k2 = new book();
+        k2.setKategori("fiksi");
+        k2.setJudul("Man Boy");
+        k2.setNamaPenulis("Aliazalea");
+        k2.setJumlahHalaman(312);
+        k2.setTahunTerbit(2022);
+
+        book k3 = new book();
+        k3.setKategori("fiksi");
+        k3.setJudul("Tiga dalam Kayu");
+        k3.setNamaPenulis("Ziggy Zezsyazeoviennazabrizkie");
+        k3.setJumlahHalaman(168);
+        k3.setTahunTerbit(2022);
+
+        book k4 = new book();
+        k4.setKategori("fiksi");
+        k4.setJudul("Tempat Paling Liar di Muka Bumi");
+        k4.setNamaPenulis("Theoresia Rumthe & Weslly Johannes");
+        k4.setJumlahHalaman(104);
+        k4.setTahunTerbit(2016);
+
+        book k5 = new book();
+        k5.setKategori("fiksi");
+        k5.setJudul("Pangeran Cilik: Le Petit Prince");
+        k5.setNamaPenulis("Antoine de Saint-Exupéry");
+        k5.setJumlahHalaman(104);
+        k5.setTahunTerbit(1979);
+        
+
+
+        t1.tampilkan();
+        t2.tampilkan();
+        t3.tampilkan();
+        t4.tampilkan();
+        t5.tampilkan();
+
+        f1.tampilkan();
+        f2.tampilkan();
+        f3.tampilkan();
+        f4.tampilkan();
+        f5.tampilkan();
+
+        s1.tampilkan();
+        s2.tampilkan();
+        s3.tampilkan();
+        s4.tampilkan();
+        s5.tampilkan();
+
+        a1.tampilkan();
+        a2.tampilkan();
+        a3.tampilkan();
+        a4.tampilkan();
+        a5.tampilkan();
+
+        p1.tampilkan();
+        p2.tampilkan();
+        p3.tampilkan();
+        p4.tampilkan();
+        p5.tampilkan();
+
+        l1.tampilkan();
+        l2.tampilkan();
+        l3.tampilkan();
+        l4.tampilkan();
+        l5.tampilkan();
+
+        k1.tampilkan();
+        k2.tampilkan();
+        k3.tampilkan();
+        k4.tampilkan();
+        k5.tampilkan();
     }
 }
