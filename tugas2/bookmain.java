ed9442363733fc9e2b9b3f4e198618a21f13f179
arifamhrni <arifamaharanisalsabil@gmail.com>
2023-03-23 23:40:05 +0700

    pemlan dua

--- tugas2/bookmain.java
+++ tugas2/bookmain.java
@@ -3,119 +3,119 @@ package tugas2;
 public class bookmain {
     public static void main(String[] args) {
         System.out.println("Kategori Teknologi");
-        book book1 = new book("Buku Dasar-Dasar Teknik Informatika", "Teknologi", "Novega Pratama Adiputra", "This is a book about Java programming.");
+        book book1 = new book("Buku Dasar-Dasar Teknik Informatika", "Teknologi", "Novega Pratama Adiputra", "Buku ini berisikan tentang dasar-dasar ilmu komputer khususnya teknik informatika beserta penjelasannya");
         book1.tampilkan();
 
-        book book2 = new book("Buku Pengantar Teknologi Informasi", "Teknologi", "Buhori Muslim", "This is a book about Java programming.");
+        book book2 = new book("Buku Pengantar Teknologi Informasi", "Teknologi", "Buhori Muslim", "Buku ini membahas tentang pengantar dari sebuah teknologi informasi beserta penjelasannya");
         book2.tampilkan();
 
-        book book3 = new book("Metode Penelitian Teknik Informatika", "Teknologi", "Ade Johar Maturidi", "This is a book about Java programming.");
+        book book3 = new book("Metode Penelitian Teknik Informatika", "Teknologi", "Ade Johar Maturidi", "Buku iini berisikan penjelasan tentang metode-metode penelitian untuj jurusan teknik informatika");
         book3.tampilkan();
 
-        book book4 = new book("Komputer Cerdas Untuk Mahasiswa Teknik Informatika", "Teknologi", "Nur Nafiiyah", "This is a book about Java programming.");
+        book book4 = new book("Komputer Cerdas Untuk Mahasiswa Teknik Informatika", "Teknologi", "Nur Nafiiyah", "Buku ini berisikan tentang penjelasan dari sebuah ilmu komuter untuk anda yang jurusan teknik informatika");
         book4.tampilkan();
 
-        book book5 = new book("Buku Aplikasi KomputeR", "Teknologi", "Dwi Krisbiantoro", "This is a book about Java programming.");
+        book book5 = new book("Buku Aplikasi Komputer", "Teknologi", "Dwi Krisbiantoro", "Buku ini berisikan tentang sebuah ilmu komputer dalam menerapkannya ke dalam sebuah aplikasi");
         book5.tampilkan();
 
 
         System.out.println("Kategori Filsafat");
-        book book6 = new book("Buku Ajar Filsafat Ilmu", "filsafat", "Drs. H. Agus Thoha, M.Si.", "This is a book about Java programming.");
+        book book6 = new book("Buku Ajar Filsafat Ilmu", "filsafat", "Drs. H. Agus Thoha, M.Si.", "Buku ini berisikan tentang bahan ajar tentang sebuah ilmu filsafat");
         book6.tampilkan();
 
-        book book7 = new book("Buku Studi Dasar Filsafat", "filsafat", "Tazkiyah Basaad", "This is a book about Java programming.");
+        book book7 = new book("Buku Studi Dasar Filsafat", "filsafat", "Tazkiyah Basaad", "Buku ini berisikan penjelasan serta penelitian tentang ilmu dasar dalam filsafat");
         book7.tampilkan();
 
-        book book8 = new book("Buku Fislafat Hukum", "filsafat", "Uri Sucipto", "This is a book about Java programming.");
+        book book8 = new book("Buku Fislafat Hukum", "filsafat", "Uri Sucipto", "Buku ini berisikan penjelasan bagaimana ilmu filsafat dilihat dari kacamata hukum");
         book8.tampilkan();
 
-        book book9 = new book("Telaah Filsafat Pendidikan", "filsafat", "Aliet Noorhayati Sutisno, Fikriyah, Windari Pandanita", "This is a book about Java programming.");
+        book book9 = new book("Telaah Filsafat Pendidikan", "filsafat", "Aliet Noorhayati Sutisno, Fikriyah, Windari Pandanita", "Buku ini berisikan penjelasan tentang bagaimana filsafat menelaah dan memandang sebuah pendiidikan");
         book9.tampilkan();
 
-        book book10 = new book("Hakikat Mencari Pengetahuan", "filsafat", "Ahmad Taufik Nasution", "This is a book about Java programming.");
+        book book10 = new book("Hakikat Mencari Pengetahuan", "filsafat", "Ahmad Taufik Nasution", "Buku ini berisikan tentang sebuah pendidikan yang dilihat dari kacamata ilmu filsafat");
         book10.tampilkan();
 
 
         System.out.println("Kategori Sejarah");
-        book book11 = new book("Di Bawah Bendera Revolusi", "sejarah", "Ir. Soekarno", "This is a book about Java programming.");
+        book book11 = new book("Di Bawah Bendera Revolusi", "sejarah", "Ir. Soekarno", "Buku yang diitulis oleh Ir. soekarno ini berisikan perjuangan bangsa indonesia");
         book11.tampilkan();
 
-        book book12 = new book("A History of Modern Indonesia", "sejarah", "Adrian Vickers", "This is a book about Java programming.");
+        book book12 = new book("A History of Modern Indonesia", "sejarah", "Adrian Vickers", "Buku ini menjelaskan tentang bagaimana sejarah indonesia dalam zaman modern seperti sekarang ini");
         book12.tampilkan();
 
-        book book13 = new book("Nusantara: Sejarah Indonesia", "sejarah", "Vlekke Bernard, Mardi Siswoko Bakti, Samsudin Berlian Zaim Rofiqi", "This is a book about Java programming.");
+        book book13 = new book("Nusantara: Sejarah Indonesia", "sejarah", "Vlekke Bernard, Mardi Siswoko Bakti, Samsudin Berlian Zaim Rofiqi", "Buku ini menjalaskan tentang bagaimana sejarah indonesia dari sebelum hingga setelah kemerdekaan");
         book13.tampilkan();
 
-        book book14 = new book("Unfinished Nation: Indonesia Before and After Soeharto", "sejarah", "Max Lane", "This is a book about Java programming.");
+        book book14 = new book("Unfinished Nation: Indonesia Before and After Soeharto", "sejarah", "Max Lane", "Buku ini menceritakan tentang bagaimana sejarah indonesia dari masa sebelum hingga sesudah soeharto menjadi presiden negara indonesia");
         book14.tampilkan();
 
-        book book15 = new book("Laut Bercerita", "sejarah", "Leila S. Chudori", "This is a book about Java programming.");
+        book book15 = new book("Laut Bercerita", "sejarah", "Leila S. Chudori", "Buku ini menceritakan tentang terkait perilaku kekejaman dan kebengisan yang dirasakan oleh kelompok aktivis mahasiswa di masa Orde Baru.");
         book15.tampilkan();
 
 
         System.out.println("Kategori Agama");
-        book book16 = new book("Nalar Tasawuf Sebagai Revolusi Pendidikan Islam", "Istania Widayati Hidayati", "John Doe", "This is a book about Java programming.");
+        book book16 = new book("Nalar Tasawuf Sebagai Revolusi Pendidikan Islam", "Istania Widayati Hidayati", "John Doe", "Buku Ini menjelaskan tentang bagaimana nalar tasawuf sebagai revolusi pendidikan islam");
         book16.tampilkan();
 
-        book book17 = new book("Komunikasi dan Dakwah Islam", "agama", "Abdul Pirol", "This is a book about Java programming.");
+        book book17 = new book("Komunikasi dan Dakwah Islam", "agama", "Abdul Pirol", "buku ini berisikan dan menjelaskan tentang bagaimana komunikasi dan dakwah islam");
         book17.tampilkan();
 
-        book book18 = new book("Murabahah", "agama", "Mukhlishin", "This is a book about Java programming.");
+        book book18 = new book("Murabahah", "agama", "Mukhlishin", "Buku ini menjelaskan tentang proses transaksi jual-beli barang ketika harga asal dan keuntungan telah diketahui dan disepakati oleh kedua belah pihak");
         book18.tampilkan();
 
-        book book19 = new book("17 Tuntunan Hidup Muslim", "agama", "Wahyono Hadi Parmono dan Ismunandar", "This is a book about Java programming.");
+        book book19 = new book("17 Tuntunan Hidup Muslim", "agama", "Wahyono Hadi Parmono dan Ismunandar", "Buku ini menjelaskan tentang bagaimana tuntutan hidup dalam seorang muslim yang baik");
         book19.tampilkan();
 
-        book book20 = new book("Bahagia Bersama Quran", "agama", "Dasep Bayu Ahyar", "This is a book about Java programming.");
+        book book20 = new book("Bahagia Bersama Quran", "agama", "Dasep Bayu Ahyar", "Buku ini menjelaskan tentang bagaimana al-quran bisa menjadi pedoman hidup kita serta kita senantiasa mencintai al-quran");
         book20.tampilkan();
 
 
         System.out.println("Kategori Psikologi");
-        book book21 = new book("Buku Bimbingan dan Konseling Belajar", "psikologi", "Abdul Saman & Agustan Arifin", "This is a book about Java programming.");
+        book book21 = new book("Buku Bimbingan dan Konseling Belajar", "psikologi", "Abdul Saman & Agustan Arifin", "Buku ini menjelaskan bagaimana seorang guru dalam membimbing dan membantu muridnya dalam belajar");
         book21.tampilkan();
 
-        book book22 = new book("Buku Psikologi Pedidikan", "psikologi", "Mahsup & Yunita Septriana Anwar", "This is a book about Java programming.");
+        book book22 = new book("Buku Psikologi Pedidikan", "psikologi", "Mahsup & Yunita Septriana Anwar", "Buku ini menjelaskan tentang bagaimana pendidikan dalam kacamata ilmu psikologi");
         book22.tampilkan();
 
-        book book23 = new book("Buku Psikologi Belajar", "psikologi", "Afi Parnawi", "This is a book about Java programming.");
+        book book23 = new book("Buku Psikologi Belajar", "psikologi", "Afi Parnawi", "buku ini menjelaskan tentang bagaimana sikap-sikap belajar dalam ilmu psikologi");
         book23.tampilkan();
 
-        book book24 = new book("Buku Psikologi Komunikasi", "psikologi", "Markus Utomo Sukendar", "This is a book about Java programming.");
+        book book24 = new book("Buku Psikologi Komunikasi", "psikologi", "Markus Utomo Sukendar", "Buku ini menjelaskan tentang bagaimana sikap-sikap komunikasi dalam ilmu psikologi");
         book24.tampilkan();
 
-        book book25 = new book("Buku Manusia Dalam Pandangan Psikologi", "psikologi", "Pasiska, M.A. Takdir Alisyahbana, M.Pd.I", "This is a book about Java programming.");
+        book book25 = new book("Buku Manusia Dalam Pandangan Psikologi", "psikologi", "Pasiska, M.A. Takdir Alisyahbana, M.Pd.I", "Buku ini menjelaskan tentang bagaimana manusia dalam pandangan ilmu psikologi");
         book25.tampilkan();
 
         System.out.println("Kategori Politik");
-        book book26 = new book("Politik Hukum di Indonesia", "politik", "Prof. Dr. Moh. Mahfud MD", "This is a book about Java programming.");
+        book book26 = new book("Politik Hukum di Indonesia", "politik", "Prof. Dr. Moh. Mahfud MD", "Buku yang ditulis oleh Mahfud MD ini menjelaskan tentang bagaimana politik hukum di Indonesia");
         book26.tampilkan();
 
-        book book27 = new book("Etika Politik", "politik", "Franz Magnis Suseno", "This is a book about Java programming.");
+        book book27 = new book("Etika Politik", "politik", "Franz Magnis Suseno", "Buku ini menjelaskan tentang bagaimana etika politik khususnya di negara Indonesia serta di kancah dunia");
         book27.tampilkan();
 
-        book book28 = new book("Dasar-Dasar Ilmu Politik", "politik", "Miriam Budiardjo", "This is a book about Java programming.");
+        book book28 = new book("Dasar-Dasar Ilmu Politik", "politik", "Miriam Budiardjo", "Buku ini menjelaskan tentang dasar-dasar ilmu politik bagi anda yang ingin mempelajari ilmu politik");
         book28.tampilkan();
 
-        book book29 = new book("Negara dan Politik Kesejahteraan", "politik", "A. Muhaimin Iskandar", "This is a book about Java programming.");
+        book book29 = new book("Negara dan Politik Kesejahteraan", "politik", "A. Muhaimin Iskandar", "Buku ini menjelaskan tentang bagaimana negara dan politik dapat hidup berdampingan dalam keejahteraan");
         book29.tampilkan();
 
-        book book30 = new book("Dramaturgi Politik Indonesia", "politik", "Muhammad Sulhan", "This is a book about Java programming.");
+        book book30 = new book("Dramaturgi Politik Indonesia", "politik", "Muhammad Sulhan", "Buku ini menjelaskan tentang dramaturgi politik khususnya politik di Indonesia");
         book30.tampilkan();
 
         System.out.println("Kategori Fiksi");
-        book book31 = new book("Brianna dan Bottomwise", "fiksi", "Andrea Hirata", "This is a book about Java programming.");
+        book book31 = new book("Brianna dan Bottomwise", "fiksi", "Andrea Hirata", "Buku ini menceritakan tentang John Musiciante yang kehilangan gitarnya karena ulah kriminal amatir");
         book31.tampilkan();
 
-        book book32 = new book("Man Boy", "fiksi", "Aliazalea", "This is a book about Java programming.");
+        book book32 = new book("Man Boy", "fiksi", "Aliazalea", "Buku ini mmenceritakan tentang Blu yang naksir berat sama Erik Pentagon. Tapi Erik juga sudah bertahun-tahun naksir cewek lain, cewek yang terlarang");
         book32.tampilkan();
 
-        book book33 = new book("Tiga dalam Kayu", "fiksi", "Ziggy Zezsyazeoviennazabrizkie", "This is a book about Java programming.");
+        book book33 = new book("Tiga dalam Kayu", "fiksi", "Ziggy Zezsyazeoviennazabrizkie", "Buku ini mengajarkan dan mengingatkan kita tentang isu yang ada di kehidupan seharian yang ada di sekitar kita");
         book33.tampilkan();
 
-        book book34 = new book("Tempat Paling Liar di Muka Bumi", "fiksi", "Theoresia Rumthe & Weslly Johannes", "This is a book about Java programming.");
+        book book34 = new book("Tempat Paling Liar di Muka Bumi", "fiksi", "Theoresia Rumthe & Weslly Johannes", "Buku ini menceritakan tentang imajinasi jiwa-jiwa yang sedang jatuh cinta. Yang berani membabi buta mencintai. Yang saling memuja dan tak peduli.");
         book34.tampilkan();
 
-        book book35 = new book("Pangeran Cilik: Le Petit Prince", "fiksi", "Antoine de Saint-Exupéry", "This is a book about Java programming.");
+        book book35 = new book("Pangeran Cilik: Le Petit Prince", "fiksi", "Antoine de Saint-Exupéry", "Buku ini mengisahkan tentang pertemuan seorang pilot dengan seorang pangeran cilik yang berasal dari luar angkasa.");
         book35.tampilkan();
     }
 }
