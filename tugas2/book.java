b37ff1aa18076a6d979eb1d403d26f0a5e176469
arifamhrni <arifamaharanisalsabil@gmail.com>
2023-03-24 09:17:50 +0700

    tugas 2 pemlan

--- tugas2/book.java
+++ tugas2/book.java
@@ -1,45 +1,45 @@
 package tugas2;
 
 public class book {
-    private String title;
-    private String category;
-    private String author;
-    private String synopsis;
-    private int synopsisWordCount;
+    private String judul;
+    private String kategori;
+    private String penulis;
+    private String sinopsis;
+    private int hitungangkasinopsis;
 
-    public book(String title, String category, String author, String synopsis) {
-        this.title = title;
-        this.category = category;
-        this.author = author;
-        this.synopsis = synopsis;
-        this.synopsisWordCount = countWords(synopsis);
+    public book(String judul, String kategori, String penulis, String sinopsis) {
+        this.judul = judul;
+        this.kategori = kategori;
+        this.penulis = penulis;
+        this.sinopsis = sinopsis;
+        this.hitungangkasinopsis = hitungangka(sinopsis);
     }
 
-    public String setTitle(String title){
-        return this.title = title;
+    public String setjudul(String judul){
+        return this.judul = judul;
     }
 
-    public String getTitle() {
-        return title;
+    public String getjudul() {
+        return judul;
     }
 
-    public String getCategory() {
-        return category;
+    public String getkategori() {
+        return kategori;
     }
 
-    public String getAuthor() {
-        return author;
+    public String getpenulis() {
+        return penulis;
     }
 
-    public String getSynopsis() {
-        return synopsis;
+    public String getsinopsis() {
+        return sinopsis;
     }
 
-    public int getSynopsisWordCount() {
-        return synopsisWordCount;
+    public int gethitungangkasinopsis() {
+        return hitungangkasinopsis;
     }
 
-    private int countWords(String text){
+    private int hitungangka(String text){
         if (text == null || text.isEmpty()) {
             return 0;
         }
@@ -48,17 +48,17 @@ public class book {
     }
 
     public book copy() {
-        book copy = new book(this.title, this.category, this.author, this.synopsis);
+        book copy = new book(this.judul, this.kategori, this.penulis, this.sinopsis);
         return copy;
     }
 
     public void tampilkan(){
         System.out.println("====================================");
-        System.out.println("kategori buku : " + getCategory());
-        System.out.println("1. Judul Buku : " + getTitle());
-        System.out.println("2. Penulis Buku : " + getAuthor());
-        System.out.println("3. Sinopsis : " + getSynopsis());
-        System.out.println("4. Menghitung jumlah kata sinopsis = " + getSynopsisWordCount());
+        System.out.println("1. kategori buku : " + getkategori());
+        System.out.println("2. Judul Buku : " + getjudul());
+        System.out.println("3. Penulis Buku : " + getpenulis());
+        System.out.println("4. Sinopsis : " + getsinopsis());
+        System.out.println("5. Menghitung jumlah kata sinopsis = " + gethitungangkasinopsis());
         System.out.println("====================================");
         System.out.println();
     }
