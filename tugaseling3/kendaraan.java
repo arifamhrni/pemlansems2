fa9e85b2ca2bd2c3290ac312890ab2fb1a8a57d4
arifamhrni <arifamaharanisalsabil@gmail.com>
2023-05-10 23:06:23 +0700

    menangis di jalan pulang

---
+++ tugaseling3/kendaraan.java
@@ -0,0 +1,34 @@
+package tugaseling3;
+
+public class kendaraan {
+    private String jenis;
+    private int kapasitas;
+    private int penumpang;
+
+    public kendaraan(String jenis, int kapasitas) {
+        this.jenis = jenis;
+        this.kapasitas = kapasitas;
+        this.penumpang = 0;
+    }
+
+    public String getJenis() {
+        return jenis;
+    }
+
+    public int getKapasitas() {
+        return kapasitas;
+    }
+
+    public int getPenumpang() {
+        return penumpang;
+    }
+
+    public void tambahPenumpang(int jumlah) {
+        if (penumpang + jumlah <= kapasitas) {
+            penumpang += jumlah;
+            System.out.println("Berhasil menambahkan " + jumlah + " penumpang ke " + jenis);
+        } else {
+            System.out.println("Kapasitas " + jenis + " tidak mencukupi");
+        }
+    }
+}
