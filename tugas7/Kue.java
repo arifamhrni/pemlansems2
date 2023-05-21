be214309e2c0be0b0c09dd3a7d9690871a5fc773
arifamhrni <arifamaharanisalsabil@gmail.com>
2023-05-21 11:54:10 +0700

    hormat kepada angin

---
+++ tugas7/Kue.java
@@ -0,0 +1,43 @@
+package tugas7;
+
+abstract class Kue {
+    protected String nama;
+    protected double harga;
+
+    public Kue(String nama, double harga) {
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public abstract double hitungHarga();
+
+    public String toString() {
+        return "Kue: " + nama + "\nHarga: Rp" + hitungHarga();
+    }
+}
+
+class KuePesanan extends Kue {
+    double berat;
+
+    public KuePesanan(String nama, double harga, double berat) {
+        super(nama, harga);
+        this.berat = berat;
+    }
+
+    public double hitungHarga() {
+        return harga * berat;
+    }
+}
+
+class KueJadi extends Kue {
+    double jumlah;
+
+    public KueJadi(String nama, double harga, double jumlah) {
+        super(nama, harga);
+        this.jumlah = jumlah;
+    }
+
+    public double hitungHarga() {
+        return harga * jumlah * 2;
+    }
+}
---
+++ tugas7/Main.java
@@ -0,0 +1,78 @@
+package tugas7;
+
+public class Main {
+    public static void main(String[] args) {
+        Kue[] daftarKue = new Kue[20];
+
+        // Isi objek KuePesanan
+        daftarKue[0] = new KuePesanan("Kue Ulang Tahun", 100000, 1);
+        daftarKue[1] = new KuePesanan("Kue Brownis", 20000, 0.5);
+        
+
+        // Isi objek KueJadi
+        daftarKue[2] = new KueJadi("Donat", 5000, 3);
+        daftarKue[3] = new KueJadi("kue sus", 4000, 5);
+        
+
+        // Tampilkan semua kue beserta jenisnya
+        System.out.println("Daftar Kue:");
+        for (Kue kue : daftarKue) {
+            if (kue != null) {
+                if (kue instanceof KuePesanan) {
+                    System.out.println("Jenis: Kue Pesanan");
+                } else if (kue instanceof KueJadi) {
+                    System.out.println("Jenis: Kue Jadi");
+                }
+                System.out.println(kue.toString());
+                System.out.println();
+            }
+        }
+
+        // Hitung total harga dari semua jenis kue
+        double totalHarga = 0.0;
+        for (Kue kue : daftarKue) {
+            if (kue != null) {
+                totalHarga += kue.hitungHarga();
+            }
+        }
+        System.out.println("Total Harga Semua Kue: Rp" + totalHarga);
+
+        // Hitung total harga dan total berat dari KuePesanan
+        double totalHargaKuePesanan = 0.0;
+        double totalBerat = 0.0;
+        for (Kue kue : daftarKue) {
+            if (kue instanceof KuePesanan) {
+                totalHargaKuePesanan += kue.hitungHarga();
+                totalBerat += ((KuePesanan) kue).berat;
+            }
+        }
+        System.out.println("Total Harga Kue Pesanan: Rp" + totalHargaKuePesanan);
+        System.out.println("Total Berat Kue Pesanan: " + totalBerat + " kg");
+
+        // Hitung total harga dan total jumlah dari KueJadi
+        double totalHargaKueJadi = 0.0;
+        double totalJumlah = 0.0;
+        for (Kue kue : daftarKue) {
+            if (kue instanceof KueJadi) {
+                totalHargaKueJadi += kue.hitungHarga();
+                totalJumlah += ((KueJadi) kue).jumlah;
+            }
+        }
+        System.out.println("Total Harga Kue Jadi: Rp" + totalHargaKueJadi);
+        System.out.println("Total Jumlah Kue Jadi: " + totalJumlah);
+
+        // Tampilkan informasi kue dengan harga terbesar
+        double hargaTerbesar = 0.0;
+        Kue kueHargaTerbesar = null;
+        for (Kue kue : daftarKue) {
+            if (kue != null && kue.hitungHarga() > hargaTerbesar) {
+                hargaTerbesar = kue.hitungHarga();
+                kueHargaTerbesar = kue;
+            }
+        }
+        if (kueHargaTerbesar != null) {
+            System.out.println("Kue dengan Harga Terbesar:");
+            System.out.println(kueHargaTerbesar.toString());
+        }
+    }
+}
